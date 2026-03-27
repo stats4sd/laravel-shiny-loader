5095ed5a12f885b308ef766c254f6b0b07a72c51
Dave Mills <dave-mills@users.noreply.github.com>
2026-03-27 15:37:31 +0000

    Add error handling to Shiny session authentication
    Refactor authenticateShiny method to include error handling for session authentication failures.

--- src/Http/Controllers/ShinyController.php
+++ src/Http/Controllers/ShinyController.php
@@ -10,37 +10,45 @@ class ShinyController
 {
     public function authenticateShiny(Request $request): JsonResponse
     {
-        $session = $request->get('session');
-        $postData = $request->get('post_data');
-
-        if (! $session) {
-            return response()->json(['error' => 'Session not found'], 404);
+        try {
+        
+            $session = $request->get('session');
+            $postData = $request->get('post_data');
+    
+            if (! $session) {
+                return response()->json(['error' => 'Session not found'], 404);
+            }
+    
+            // Look for the file with the same session name.
+            $file = fopen(config('shiny-loader.app-path').'/.sessions/'.$session, 'r');
+    
+            // Get the POST url from that file
+            $url = [];
+    
+            while (! feof($file)) {
+                $url[] = fgets($file);
+            }
+    
+            fclose($file);
+    
+            // format the URL correctly
+            $finalUrl = str_replace("\n", '', $url[0]);
+            $finalUrl = config('services.shiny.rdmt-demo-url').$finalUrl;
+            $finalUrl = str_replace("\n", '', $finalUrl);
+    
+            // Append the auth_key to the post data
+            $postData['auth_key'] = config('shiny-loader.auth-key');
+    
+            
+    
+            Http::post(str_replace("\n", '', $finalUrl), $postData)
+                ->throw()
+                ->json();
         }
-
-        // Look for the file with the same session name.
-        $file = fopen(config('shiny-loader.app-path').'/.sessions/'.$session, 'r');
-
-        // Get the POST url from that file
-        $url = [];
-
-        while (! feof($file)) {
-            $url[] = fgets($file);
+        catch (\Exception $e) {
+            return response()->json(['error' => 'Shiny session authentication failed', 'url' => $finalUrl], 419);
         }
-
-        fclose($file);
-
-        // format the URL correctly
-        $finalUrl = str_replace("\n", '', $url[0]);
-        $finalUrl = config('services.shiny.rdmt-demo-url').$finalUrl;
-        $finalUrl = str_replace("\n", '', $finalUrl);
-
-        // Append the auth_key to the post data
-        $postData['auth_key'] = config('shiny-loader.auth-key');
-
-        Http::post(str_replace("\n", '', $url[0]), $postData)
-            ->throw()
-            ->json();
-
+        
         return response()->json([
             'success' => 'Shiny session authenticated',
         ]);
