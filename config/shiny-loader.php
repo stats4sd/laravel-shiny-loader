0511bd5d5048eb2cc9938b00f56c703f9f6c4719
dave-mills <5711101+dave-mills@users.noreply.github.com>
2026-05-31 12:03:17 +0000

    Fix styling

--- config/shiny-loader.php
+++ config/shiny-loader.php
@@ -5,8 +5,8 @@ return [
     'app-path' => env('SHINY_APP_PATH', base_path('shiny')),
     'auth-key' => env('SHINY_AUTH_KEY', 'change-me'),
 
-    ## Add the urls to every shiny app that will be embedded here, and the .env KEYs that hold them, e.g.:
-    # 'monitoring-app-url' => env('SHINY_APP_URL_MONITORING', 'http://127.0.0.1:7008'),
-    # 'analysis-app-url' => env('SHINY_APP_URL_ANALYSIS', 'http://127.0.0.1:7009'),
+    // # Add the urls to every shiny app that will be embedded here, and the .env KEYs that hold them, e.g.:
+    // 'monitoring-app-url' => env('SHINY_APP_URL_MONITORING', 'http://127.0.0.1:7008'),
+    // 'analysis-app-url' => env('SHINY_APP_URL_ANALYSIS', 'http://127.0.0.1:7009'),
 
 ];
